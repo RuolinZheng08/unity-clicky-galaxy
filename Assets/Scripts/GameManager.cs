119c24ec4c1cb0311f8ea7b14a2a96df660aa996
Lynn Zheng <ruolinzheng@uchicago.edu>
2021-04-30 14:19:15 -0700

    Add sprites to grid

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    int gridDimension = 8;
+    float pixelScale = 1;
+
+    public List<Sprite> sprites;
+    public GameObject cellPrefab;
+
+    // logical representation of cells, [0, 0] is at bottom-left
+    // grows upward and to the right
+    GameObject[,] grid;
+
+    // GUI
+
+    // Start is called before the first frame update
+    void Start() {
+        grid = new GameObject[gridDimension, gridDimension];
+        InitGrid();
+    }
+
+    void InitGrid() {
+        float center = gridDimension * pixelScale / 2;
+        Vector3 gridCenter = new Vector3(center, center, 0);
+        Vector3 gridOffset = transform.position - gridCenter;
+
+        for (int row = 0; row < gridDimension; row++) {
+            for (int col = 0; col < gridDimension; col++) {
+                GameObject cell = Instantiate(cellPrefab);
+                // set parent to be the grid
+                cell.transform.parent = transform;
+                // set position to draw
+                Vector3 cellPosition = new Vector3(col * pixelScale, row * pixelScale, 0);
+                Vector3 cellPositionOffseted = cellPosition + gridOffset;
+                cell.transform.position = cellPositionOffseted;
+                // set sprite
+                cell.GetComponent<SpriteRenderer>().sprite = GetRandomSprite(sprites);
+                // set logical position, i.e., indices in script
+                // cell.GetComponent<CellController>().indices = new Vector2Int(row, col);
+
+                grid[row, col] = cell;
+            }
+        }
+    }
+
+    Sprite GetRandomSprite(List<Sprite> sprites) {
+        int idx = Random.Range(0, sprites.Count);
+        return sprites[idx];
+    }
+
+    Sprite GetRandomSpriteAtIndices(int row, int col) {
+        return null;
+    }
+}
